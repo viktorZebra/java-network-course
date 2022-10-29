d83a2227c76a32e97b7a7f702f4b179825a0fe6b
19698720 <AONaydenyshev@sberbank.ru>
2022-10-29 14:07:37 +0300

    feature/lesson-1 задание первого урока готово к проверке

---
+++ lesson1/src/main/java/lesson1.java
@@ -0,0 +1,8 @@
+public class lesson1 {
+
+    public static void main(String[] args) {
+        NetworkInfo networkInfo = new NetworkInfo();
+
+        networkInfo.printAllNetworkInterface();
+    }
+}
